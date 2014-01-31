1ebb69f9655300c2d68bff8325c74f419e4d8d72
Ned Gulley <gulley@gmail.com>
2014-01-31 15:03:28 -0500

    Initial check in

---
+++ getdbtables.m
@@ -0,0 +1,24 @@
+function getdbtables(conn)
+%GETDBTABLES
+%   getdbtables(conn)
+
+d = dmd(conn);
+ctg = get(conn,'Catalog');
+tinfo = tables(d,ctg,'');
+
+for n = 1:size(tinfo,1),
+    table = tinfo{n,1};
+    % The "limit 1" construct is from MySQL
+    ex = exec(conn,['select * from ' table ' limit 1']);
+    ex = fetch(ex,1);
+    a = attr(ex);
+    tabfields = {a.fieldName};
+    close(ex);
+    
+    fprintf(1,'\nTABLE: %s\n',table);
+    for m = 1:length(tabfields),
+        fprintf(1,'   %s\n',tabfields{m});
+    end
+end
+
+
