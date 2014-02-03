0d9373ab3618eea0f07172068234d0defe49b195
Ned Gulley <gulley@gmail.com>
2014-02-03 15:59:25 -0500

    Include all the contest IDs
    I had left off the last three.

--- getAllContestIds.m
+++ getAllContestIds.m
@@ -3,4 +3,5 @@ function contestIdList = getAllContestIds
 % They were added to the new database in a strange order. Also, there are a
 % number of contests that should be left out.
 
-contestIdList = [3 4 8 18 11 19 24 12 6 16 10 17 7 13 9 14 27 2 30 32];
+contestIdList = [3 4 8 18 11 19 24 12 6 16 10 17 7 ...
+    13 9 14 27 2 30 32 34 36 38];
