0c49b38e6172f5c33d34307a72661f21fbea4282
Ned Gulley <gulley@gmail.com>
2014-02-04 16:12:50 -0500

    Adding contents and improving help

---
+++ Contents.m
@@ -0,0 +1,24 @@
+% CONTEST-ANALYSIS
+%
+% Getting Started
+%   setup                    - Make sure this directory is on the path
+%
+% Data Access
+%   connectToContestDatabase - connectToContestDatabase
+%   getAllContestIds         - This function returns the contest IDs in the right order.
+%   getContestData           - getContestData  Get the data associated with a contest from the database
+%   getContestMetaData       - 
+%   getdbtables              - Display database tables
+%   getSubmissionCode        - getSubmissionCode(id,db)
+%
+% File Manipulation
+%   codesplit                - Split the code into separate lines
+%   hashFiles                - hashFiles
+%
+% Plots
+%   authorSharePlot          - Show author "code share" in an area plot
+%   stepchart                - 
+%   timeplot                 - 
+%
+% Other Files
+%   rankAtDebut              - rankAtDebut  Calculate the initial rank for all entries
--- authorSharePlot.m
+++ authorSharePlot.m
@@ -1,30 +1,33 @@
 function a = authorSharePlot(d,firstTimeIndexList)
-
-% The authorMap maps from entry_id to author_id
-authorMap = [d.author_id];
-
-authorsOfAllEntries = [d.author_id];
-[~,ix] = unique(authorsOfAllEntries,'first');
-% This keeps the authors in order of first appearance
-authorList = authorsOfAllEntries(ix);
-numAuthors = length(authorList);
-numEntries = length(d);
-a = zeros(numAuthors,numEntries);
-
-for entryCount = 1:numEntries
-    linesInLeader = d(entryCount).lines;
-    entriesInLeader = firstTimeIndexList(linesInLeader);
-    authorsInLeader = authorMap(entriesInLeader);
+    %AUTHORSHAREPLOT Show author "code share" in an area plot
+    
+    % The authorMap maps from entry_id to author_id
+    authorMap = [d.author_id];
+    
+    authorsOfAllEntries = [d.author_id];
+    [~,ix] = unique(authorsOfAllEntries,'first');
+    % This keeps the authors in order of first appearance
+    authorList = authorsOfAllEntries(ix);
+    numAuthors = length(authorList);
+    numEntries = length(d);
+    a = zeros(numAuthors,numEntries);
+    
+    for entryCount = 1:numEntries
+        linesInLeader = d(entryCount).lines;
+        entriesInLeader = firstTimeIndexList(linesInLeader);
+        authorsInLeader = authorMap(entriesInLeader);
         
-    for authorCount = 1:numAuthors        
-        a(authorCount,entryCount) = sum(authorsInLeader==authorList(authorCount));
+        for authorCount = 1:numAuthors
+            a(authorCount,entryCount) = sum(authorsInLeader==authorList(authorCount));
+        end
     end
+    
+    area(a')
+    xlim([0 numEntries])
+    xlabel('Index of the Current Leading Entry')
+    ylabel('Number of Lines in the Leading Entry')
+    
+    legendStr = cellfun(@num2str,num2cell(authorList),'UniformOutput',false);
+    legend(legendStr,'Location','EastOutside','FontSize',6)
+    
 end
-
-area(a')
-xlim([0 numEntries])
-xlabel('Index of the Current Leading Entry')
-ylabel('Number of Lines in the Leading Entry')
-
-legendStr = cellfun(@num2str,num2cell(authorList),'UniformOutput',false);
-legend(legendStr,'Location','EastOutside','FontSize',6)
